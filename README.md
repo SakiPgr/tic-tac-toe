684fe0128b692f3526c0ca13cc801d49300c71c2
SakiPgr <realsaki@hotmail.gr>
2018-06-08 00:05:22 +0300

    changed media sizes and added the logic to add x and o

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 A simple tictactoe game
 
 I don't think that there is much to be said at this point. Just as the name says a simple tictactoe game.
+
+07 June 2018 - Keep trying things and to add at least one new thing to this project
--- tictactoe.js
+++ tictactoe.js
@@ -1,7 +1,16 @@
 var td = document.querySelectorAll('td');
+var player = true;
 
 for (var i = 0; i <= td.length; i++) {
   td[i].addEventListener("click" , function() {
-    this.style.visibility = 'hidden';
+    if (this.textContent === "" && player) {
+      this.textContent = 'x';
+      player = !player;
+    } else if (this.textContent === "" && !player) {
+      this.textContent = 'o';
+      player = !player;
+    } else {
+      console.log(this.textContent)
+    }
 })
 };
