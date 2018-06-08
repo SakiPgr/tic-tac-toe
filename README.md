3fda3a8cae49230e30d11541301577fdcc26ffd9
SakiPgr <realsaki@hotmail.gr>
2018-06-08 22:39:05 +0300

    small changes for media sizes and added button and turn field

--- README.md
+++ README.md
@@ -3,4 +3,6 @@ A simple tictactoe game
 
 I don't think that there is much to be said at this point. Just as the name says a simple tictactoe game.
 
-07 June 2018 - Keep trying things and to add at least one new thing to this project
+07 June 2018 - Keep trying things and to add at least one new thing to this project.
+
+08 June 2018 - Kept changing small things and added button to reset the game and a field to show whos turn it is.
--- tictactoe.js
+++ tictactoe.js
@@ -1,13 +1,16 @@
 var td = document.querySelectorAll('td');
 var player = true;
+var turnPlayer = document.querySelector("strong");
 
 for (var i = 0; i <= td.length; i++) {
   td[i].addEventListener("click" , function() {
     if (this.textContent === "" && player) {
       this.textContent = 'x';
+      turnPlayer.textContent = "Second"
       player = !player;
     } else if (this.textContent === "" && !player) {
       this.textContent = 'o';
+      turnPlayer.textContent = "First"
       player = !player;
     } else {
       console.log(this.textContent)
