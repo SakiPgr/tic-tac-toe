10cc70f484ddb3651a8ffaf170d2e525502c2dda
SakiPgr <realsaki@hotmail.gr>
2018-06-10 21:46:13 +0300

    some js changes. need to find win conditions

--- README.md
+++ README.md
@@ -6,3 +6,7 @@ I don't think that there is much to be said at this point. Just as the name says
 07 June 2018 - Keep trying things and to add at least one new thing to this project.
 
 08 June 2018 - Kept changing small things and added button to reset the game and a field to show whos turn it is.
+
+09 June 2018 - Due to lack of time added only some media querys concerning height adjustments.
+
+10 June 2018 - Some additions to the logic of the game
--- tictactoe.js
+++ tictactoe.js
@@ -1,20 +1,42 @@
 var td = document.querySelectorAll('td');
 var player = true;
 var turnPlayer = document.querySelector("strong");
+var winner = document.getElementById("winner");
+var newGame = document.getElementById("newGame");
 
-for (var i = 0; i <= td.length; i++) {
-  td[i].addEventListener("click" , function() {
-    if (this.textContent === "" && player) {
-      this.textContent = 'x';
-      turnPlayer.textContent = "Second"
-      player = !player;
-    } else if (this.textContent === "" && !player) {
-      this.textContent = 'o';
-      turnPlayer.textContent = "First"
-      player = !player;
-    } else {
-      console.log(this.textContent)
-    }
-})
+newGame.addEventListener("click", function(){
+  for (var i = 0; i < td.length; i++) {
+    td[i].textContent = "";
+  }
+});
+
+function game() {
+  for (var i = 0; i < td.length; i++) {
+    td[i].addEventListener("click", function() {
+      if (this.textContent === "" && player) {
+        this.textContent = 'x';
+        turnPlayer.textContent = "Second"
+        winningTheGame()
+        player = !player;
+      } else if (this.textContent === "" && !player) {
+        this.textContent = 'o';
+        turnPlayer.textContent = "First"
+        player = !player;
+      } else {
+        alert("There is already a "+ '"' + this.textContent +
+        '"' + " there")
+      }
+  })}
 };
 
+// this needs some more testing to find the right way
+function winningTheGame() {
+  if (td[0].textContent === "x" &&
+      td[1].textContent === "x" &&
+      td[2].textContent === "x") {
+    alert("You won!")
+  }
+}
+
+
+game();
