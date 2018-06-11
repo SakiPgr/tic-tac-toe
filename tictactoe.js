c8127f5dddbe38a79c9bba827126e6142ba6c189
SakiPgr <realsaki@hotmail.gr>
2018-06-11 10:36:25 +0300

    found the conditions, need to end it correctly

--- tictactoe.js
+++ tictactoe.js
@@ -3,11 +3,22 @@ var player = true;
 var turnPlayer = document.querySelector("strong");
 var winner = document.getElementById("winner");
 var newGame = document.getElementById("newGame");
+var winCondition = [[td[0], td[1], td[2]],
+[td[3], td[4], td[5]],
+[td[6], td[7], td[8]],
+[td[0], td[3], td[6]],
+[td[1], td[4], td[7]],
+[td[2], td[5], td[8]],
+[td[0], td[4], td[8]],
+[td[2], td[4], td[6]]];
 
 newGame.addEventListener("click", function(){
   for (var i = 0; i < td.length; i++) {
     td[i].textContent = "";
   }
+  turnPlayer.textContent = "First";
+  player = true;
+  winner.textContent = "";
 });
 
 function game() {
@@ -16,27 +27,30 @@ function game() {
       if (this.textContent === "" && player) {
         this.textContent = 'x';
         turnPlayer.textContent = "Second"
-        winningTheGame()
-        player = !player;
       } else if (this.textContent === "" && !player) {
         this.textContent = 'o';
         turnPlayer.textContent = "First"
-        player = !player;
       } else {
         alert("There is already a "+ '"' + this.textContent +
         '"' + " there")
+        player = !player;
       }
+      player = !player;
+      winningTheGame()
   })}
 };
 
-// this needs some more testing to find the right way
 function winningTheGame() {
-  if (td[0].textContent === "x" &&
-      td[1].textContent === "x" &&
-      td[2].textContent === "x") {
-    alert("You won!")
+  for (var i = 0; i < winCondition.length; i++) {
+    if (winCondition[i][0].textContent !== "") {
+      if (winCondition[i][0].textContent ===
+          winCondition[i][1].textContent &&
+          winCondition[i][0].textContent ===
+          winCondition[i][2].textContent) {
+        winner.textContent = turnPlayer.textContent;
+      }
+    }
   }
 }
-
 
 game();
